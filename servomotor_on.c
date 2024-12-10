b7b1007896cc1c923c5b49c4e2df75b2b56f3c26
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-12-10 21:25:26 +0900

    Add files via upload

---
+++ servomotor_on.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+void* rotate_Servo_on(void* arg){
+    pinMode(18, PWM_OUTPUT);
+    float duty = (0.5 + ((90 + 90.0) / 90)) * 100;
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetRange(2000);
+    pwmSetClock(192);
+    pwmWrite(18, duty);
+}
---
+++ thread.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include "twilio.h"
+
+int share_var = 1;
+void *producerFunc(void *); //생산자 쓰레드 실행 함수
+void *consumerFunc(void *); //소비자 쓰레드 실행 함수
+
+void* consumerFunc(void* arg){
+    for(int i = 0; i<2 ; i++){
+        char* account_sid="AC1751320dc8094d593bd610c4c36bafb8";
+        char* auth_token="04ee95eec4c5c42b196bf1c9e1e0d21c";
+        char* from_number="+19787974155";
+        char* to_number = "+821029588856";
+        char* message = "hi";
+        char *picture_url = "https://demo.twilio.com/logo.png";
+        bool verbose = true;
+        twilio_send_message(account_sid, auth_token, from_number,to_number, message,picture_url , true);
+        
+
+        if (twilio_send_message(account_sid,
+                                auth_token,
+                                message,
+                                from_number,
+                                to_number,
+                                picture_url,
+                                verbose
+        ) == true) {
+            ;
+        } else {
+            ;
+        }
+        sleep(1);
+    }
+    return NULL;
+}
