e005f0ed7ac5cf7065e5fc29e506f700ba911641
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-12-11 20:17:02 +0900

    endend

---
+++ Main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <stdlib.h>
+#include "global.h"
+#include "twilio.h"
+
+bool BtnState;
+int prevState = 0;
+int currState = 0;
+
+pthread_mutex_t mid;
+int main(){
+    BtnState = FALSE;
+    wiringPiSetupGpio();
+    pinMode(17, INPUT);
+    pthread_t sirenPthread;
+    pthread_t cameraPthread;
+    pthread_t ServoOnPthread;
+    pthread_t ServoOffPthread;
+    pthread_t mailPthread;
+    int r_val;
+    while (TRUE)
+    {
+        currState = digitalRead(17);
+
+        if(currState == 0 && prevState == 1){
+            BtnState = !BtnState;
+
+            if(BtnState){
+                pthread_create(&sirenPthread, NULL, StartSiren, NULL);
+                pthread_create(&cameraPthread, NULL, handle_button_press, NULL);
+                pthread_create(&ServoOnPthread, NULL, rotate_Servo_on, NULL);
+                pthread_create(&mailPthread, NULL, consumerFunc, NULL);
+                if(r_val < 0){
+                    perror("pthread_create() error\n");
+                    exit(0);
+                }
+            }
+            else if(!BtnState){
+                pthread_create(&ServoOffPthread, NULL, rotate_Servo_off, NULL);
+            }
+            delay(200);
+        }
+        prevState = currState;
+    }
+    
+    return 0;
+}
---
+++ Siren.c
@@ -0,0 +1,36 @@
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <softTone.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "global.h"
+
+void* StartSiren(void* args) {
+    int gpio1 = 20, gpio2 = 16;
+    pinMode(gpio1, OUTPUT);                                                                                                           
+    softToneCreate(gpio2);
+    softPwmCreate(gpio1, 0, 100);
+    int val = 300;
+    while(BtnState){
+        int i;
+        for(i = 0; i < 100; i++){
+            val = val + 4;
+            softPwmWrite(gpio1, i);
+            softToneWrite(gpio2, val);
+            delay(5);
+        }
+        for(; i > 0; i--){
+            val = val - 4;
+            softPwmWrite(gpio1, i);
+            softToneWrite(gpio2, val);
+            delay(5);
+        }
+    }
+    EndSiren(gpio1, gpio2);
+    return NULL;
+}
+
+void EndSiren(int gpio1, int gpio2){
+    softPwmWrite(gpio1, 0);
+    softToneWrite(gpio2, 0);
+}
